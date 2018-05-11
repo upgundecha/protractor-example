1976d8ce1a54ac13563567e081a098201df3b827
Unmesh Gundecha <upgundecha@gmail.com>
2018-05-11 23:54:18 +0530

    Added lists of elements example

--- spec.js
+++ spec.js
@@ -24,6 +24,7 @@ describe('Protractor Demo App', function () {
     var secondNumber = element(by.model('second'));
     var goButton = element(by.id('gobutton'));
     var latestResult = element(by.binding('latest'));
+    
 
     beforeEach(function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
@@ -50,5 +51,28 @@ describe('Protractor Demo App', function () {
     it('should read the value from an input', function() {
         firstNumber.sendKeys(1);
         expect(firstNumber.getAttribute('value')).toEqual('1');
-      });
+    });
+
+
+    // Iteration 3 - lists of elements
+    var history = element.all(by.repeater('result in memory'));
+
+    function add(a, b) {
+        firstNumber.sendKeys(a);
+        secondNumber.sendKeys(b);
+        goButton.click();
+    }
+
+    it('should have a history', function(){
+        add(1, 2);
+        add(3, 4);
+        
+        expect(history.last().getText()).toContain('1 + 2');
+        
+        expect(history.count()).toEqual(2);
+        
+        add(5, 6);
+
+        expect(history.count()).toEqual(3);
+    });
 });
