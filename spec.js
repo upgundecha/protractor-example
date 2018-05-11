48eaac79626ce1eb30d6d66b95763cc7d1d08968
Unmesh Gundecha <upgundecha@gmail.com>
2018-05-11 23:44:02 +0530

    Add a more test for calculator

--- spec.js
+++ spec.js
@@ -1,5 +1,7 @@
 describe('Protractor Demo App', function () {
     
+    // Iteration 1 - Simple Scenarios
+    /*
     it('should have a tile', function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
         expect(browser.getTitle()).toEqual('Super Calculator');
@@ -14,5 +16,39 @@ describe('Protractor Demo App', function () {
 
         expect(element(by.binding('latest')).getText()).toEqual('3');
     });
+    */
 
+    // Iteration 2 - Multiple Scenarios
+
+    var firstNumber = element(by.model('first'));
+    var secondNumber = element(by.model('second'));
+    var goButton = element(by.id('gobutton'));
+    var latestResult = element(by.binding('latest'));
+
+    beforeEach(function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    });
+
+    it('should add one and two', function() {
+        firstNumber.sendKeys(1);
+        secondNumber.sendKeys(2);
+
+        goButton.click();
+
+        expect(latestResult.getText()).toEqual('3');
+    });
+
+    it('should add four and six', function() {
+        firstNumber.sendKeys(4);
+        secondNumber.sendKeys(6);
+
+        goButton.click();
+
+        expect(latestResult.getText()).toEqual('10');
+    });
+
+    it('should read the value from an input', function() {
+        firstNumber.sendKeys(1);
+        expect(firstNumber.getAttribute('value')).toEqual('1');
+      });
 });
