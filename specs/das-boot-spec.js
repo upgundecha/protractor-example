6de96a7bae79da4c0a0da58e32c7c931fc24b8aa
Unmesh Gundecha <upgundecha@gmail.com>
2018-05-21 20:07:47 +0530

    Parameterized test

--- specs/das-boot-spec.js
+++ specs/das-boot-spec.js
@@ -1,7 +1,9 @@
 var angularHomepage = require('../pages/angular-home-page.js');
+var using = require('jasmine-data-provider');
+var testData = require('../testdata/ships.json')
 
 describe('Das Boot Home Page', function () {
-    it('should display menu', function () {
+    it('should display Home page', function () {
         browser.get('http://localhost:9999/index.html');
 
         var homeLink = element(by.linkText("Home"));
@@ -15,45 +17,59 @@ describe('Das Boot Home Page', function () {
 });
 
 describe('Das Boot Shipwrecks List', function () {
-    it('should add a new Shipwreck', function () {
+    beforeEach(function () {
         browser.get('http://localhost:9999/index.html');
-        
-        var shipwrecksLink = element(by.linkText('Shipwrecks'));
-        shipwrecksLink.click();
+    });
 
-        var addNewButton = element(by.linkText('Add New Shipwreck'));
-        addNewButton.click();
+    // we will use this counter
+    var shipsCounter = 0; 
+    
+    using(testData, function(inputData){
+        it(`should add a new ship > ${inputData.name}`, function () {
 
-        $('#name').sendKeys('U Boat 66');
-        $('#description').sendKeys('German U Boat 66');
-        $('#condition').sendKeys('Fair');
-        $('#yearDiscovered').sendKeys('2000');
-        $('#depth').sendKeys('1000');
-        $('#latitude').sendKeys('12.00')
-        $('#longitude').sendKeys('12.00')
-        $("input[value='Save']").click();
+            var shipwrecksLink = element(by.linkText('Shipwrecks'));
+            shipwrecksLink.click();
 
-        // check newly added Shipwreck
-        var swList = element.all(by.repeater('shipwreck in shipwrecks'));
-        expect(swList.count()).toEqual(1, 'Shipwrecks count did not match!');
-        expect(swList.get(0).getText()).toEqual('U Boat 66 German U Boat 66 2000 View Delete');
+            var addNewButton = element(by.linkText('Add New Shipwreck'));
+            addNewButton.click();
+
+            $('#name').sendKeys(inputData.name);
+            $('#description').sendKeys(inputData.description);
+            $('#condition').sendKeys(inputData.condition);
+            $('#yearDiscovered').sendKeys(inputData.yearDiscovered);
+            $('#depth').sendKeys(inputData.depth);
+            $('#latitude').sendKeys(inputData.latitude)
+            $('#longitude').sendKeys(inputData.longitude)
+            $("input[value='Save']").click();
+
+            // check newly added Shipwreck
+            var swList = element.all(by.repeater('shipwreck in shipwrecks'));
+            var name = swList.get(shipsCounter).element(by.binding('shipwreck.name'));
+            var description = swList.get(shipsCounter).element(by.binding('shipwreck.description'));
+            var year = swList.get(shipsCounter).element(by.binding('shipwreck.year'));
+
+            expect(name.getText()).toEqual(inputData.name);
+            expect(description.getText()).toEqual(inputData.description);
+            expect(year.getText()).toEqual(inputData.yearDiscovered);
+
+            shipsCounter++;
+        });
     });
 
     it('should delete Shipwreck', function () {
-        browser.get('http://localhost:9999/index.html');
-        
-        var shipwrecksLink = element(by.linkText('Shipwrecks'));
+        var shipwrecksLink = element(by.linkText('Shipwrecks'));``
         shipwrecksLink.click();
     
         var swList = element.all(by.repeater('shipwreck in shipwrecks'));
         
-        expect(swList.count()).toEqual(1, 'Shipwrecks count did not match!');
-        expect(swList.get(0).getText()).toEqual('U Boat 66 German U Boat 66 2000 View Delete');
+        expect(swList.count()).toEqual(shipsCounter, 
+            'Shipwrecks count did not match!');
         
         var deleteButton = swList.get(0).element(by.linkText('Delete'))
         deleteButton.click();
         browser.switchTo().alert().accept();
         
-        expect(swList.count()).toEqual(0, 'Shipwrecks count did not match post delete!');
+        expect(swList.count()).toEqual(shipsCounter - 1, 
+            'Shipwrecks count did not match post delete!');
     });
 });
